17f560731ef4dd15c7e8cd133b534f09e10dfd05
Fale Datapage <88858959+faledatapage@users.noreply.github.com>
2021-10-07 16:14:17 -0300

    Update Index.js

--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -4,6 +4,7 @@ export default {
     return {
       text: '',
       item: 100,
+      dados: [[], [], [0]],
       cargo: [
         {
           label: 'Diretor',
@@ -18,26 +19,14 @@ export default {
           value: 'financeiro'
         }
       ],
-      dados: []
+      nome: null,
+      cnpj: null,
+      telefone: '',
+      cep: '',
+      endereco: null
     }
   },
   mounted () {
-    this.dados.push([])
-    this.dados.push([])
-    this.dados[0].push({ oi: 'ola1' })
-    this.dados[0].push({ oi: 'ola2' })
-    this.dados[1].push({ cnae: '22838379', atividade: 'Comércio varejista de mercadorias em geral, com predominância de produtos.' })
-    this.dados[1].push({ cnae: '23823723', atividade: 'Comércio varejista de livros' })
-    this.dados[1].push({ cnae: '323i2832', atividade: 'Comércio varejista de CDS' })
-    this.dados[1].push({ cnae: '43283283', atividade: 'Comércio varejista de Eletronicos' })
-    this.dados[1].push({ cnae: '32837272', atividade: 'Comércio varejista de Cortinas' })
-    console.log(this.dados)
   },
-  methods: {
-    add () {
-      // this.dados[1].push({ cnae: 'ola' + this.item, atividade: this.item + ' Atividades nome' })
-      this.item++
-      this.dados[1][2] = { cnae: 'ola' + this.item, atividade: 'Comércio varejista de livros' }
-    }
-  }
+  methods: {}
 }
